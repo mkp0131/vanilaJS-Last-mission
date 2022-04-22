00692382e561bb86e0b5205cfd0e48b88531292a
test <test>
2022-04-23 00:49:57 +0900

    api 키 받아오는 api 생성

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,34 @@
 {
-  const API_KEY = secrets.WEATHER_API_KEY;
+  const getWeather = (API_KEY) => {
+    navigator.geolocation?.getCurrentPosition(({ coords, timestamp }) => {
+      const lat = coords.latitude;
+      const lon = coords.longitude;
 
-  navigator.geolocation?.getCurrentPosition(({ coords, timestamp }) => {
-    const lat = coords.latitude;
-    const lon = coords.longitude;
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((json) => {
+          const weather = json.weather[0].description;
+          const temp = json.main.temp;
+          const location = json.name;
+          weatherHTML.innerHTML = `${weather.toUpperCase()} ${temp}°C<br/><span>${location.toUpperCase()}</span>`;
+          weatherHTML.style.opacity = 1;
+        })
+        .catch((err) => {
+          console.error('에러발생', err);
+        });
+    });
+  };
 
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((json) => {
-        const weather = json.weather[0].description;
-        const temp = json.main.temp;
-        const location = json.name;
-        weatherHTML.innerHTML = `${weather.toUpperCase()} ${temp}°C<br/><span>${location}</span>`;
-        weatherHTML.style.opacity = 1;
-      });
-  });
+  fetch('//api.lililli.kr/weather_api_github_io.php')
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      getWeather(res.api_key);
+    })
+    .catch((err) => {
+      console.error('에러발생', err);
+    });
 }
